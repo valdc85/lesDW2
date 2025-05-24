e2c3969b6b66b2502818c387643030d6cdf3d1b0
Valérie De Clercq <150483388+valdc85@users.noreply.github.com>
2025-05-25 01:23:37 +0200

    lynx testen

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 docker image build -t demo-site .
-docker run -dt -p 81:80 --name opdracht6 demo-site
-docker container ls -a
+docker run -d -p 81:80 --name opdracht6 demo-site
+docker exec -it opdracht6 bash
+apt update
+apt install -y lynx
+lynx http://localhost:81/
+
 
