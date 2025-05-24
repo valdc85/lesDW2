2addcfdacbea6c2b3f8973ed2d9ba67f56ac3c6f
Valérie De Clercq <150483388+valdc85@users.noreply.github.com>
2025-05-25 01:31:17 +0200

    modified:

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -2,9 +2,5 @@
 
 docker image build -t demo-site .
 docker run -d -p 81:80 --name opdracht6 demo-site
-docker exec -it opdracht6 bash
-apt update
-apt install -y lynx
-lynx http://localhost:81/
 
 
