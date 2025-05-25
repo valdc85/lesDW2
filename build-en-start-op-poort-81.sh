27aeecc3e4c1c8a485f87cdcb6e7cdd6f88c86b0
Valérie De Clercq <150483388+valdc85@users.noreply.github.com>
2025-05-25 02:00:37 +0200

    laatste image

--- build-en-start-op-poort-81.sh
+++ build-en-start-op-poort-81.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker image build -t demo-site .
-docker run -d -p 81:80 --name opdracht6 demo-site
+docker run -d -p 81:80 --name opdracht6 demo-site:latest
+# docker container start opdracht6
 
 
